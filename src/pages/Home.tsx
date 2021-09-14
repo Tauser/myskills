0490122eea6e387e5c74d3ebc93e28b24ad467cf
Tauser <tauser@gmail.com>
2021-09-14 09:44:23 -0300

    Adicionando e Typescript  e refactory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -11,21 +11,57 @@ import {
 
 } from 'react-native';
 import { Button } from "../components/Button";
-import { SkillCard } from "./SkillCard";
+import { SkillCard } from "../components/SkillCard";
+
+interface SkillData {
+  id: string;
+  name: string;
+}
 
 export function Home() {
 
   const [newSkill, setNewSkill] = useState('');
-  const [mySkills, setMySkills] = useState([]);
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [gretting, setGretting] = useState('');
+
 
   function handleAddNewSkill() {
-    setMySkills(oldState => [...oldState, newSkill]);
+    const data = {
+      id: String(new Date().getTime()),
+      name: newSkill,
+    }
+    setMySkills(oldState => [...oldState, data]);
   }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ))
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGretting('Good morning');
+    }
+    else if (currentHour >= 12 && currentHour < 18) {
+      setGretting('Good afternoon');
+    }
+    else {
+      setGretting('Good night');
+    }
+
+  }, [])
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bem Vindo, Tauser </Text>
+      <Text style={styles.greethings} >
+        {gretting}
+      </Text>
+
       <TextInput
         style={styles.input}
         placeholder="New skill"
@@ -33,16 +69,16 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill} />
+      <Button onPress={handleAddNewSkill} title="Add" />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>
         My Skills
       </Text>
       <FlatList
         data={mySkills}
-        keyExtractor={item => item}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <SkillCard skill={item} />
+          <SkillCard skill={item.name} onPress={() => handleRemoveSkill(item.id)} />
         )}
       />
     </View>
@@ -69,6 +105,11 @@ const styles = StyleSheet.create({
     marginTop: 30,
     borderRadius: 7,
     marginBottom: 20,
+  },
+  greethings: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: 'bold',
   },
 
 })
