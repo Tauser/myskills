72481776adc0f974cddf23f03a9dd16a6da98841
Tauser <tauser@gmail.com>
2021-09-14 03:32:26 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { Home } from './src/pages/Home';
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <Text>React Native</Text>
-    </View>
-  )
+  return <Home />
 }
